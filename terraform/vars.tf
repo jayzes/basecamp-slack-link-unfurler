2e3df96084757e2db8e8a0016caedd641a556e34
Jay Zeschin <jayzes@users.noreply.github.com>
2023-04-17 22:29:32 -0600

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "slack_access_token" {
+  description = "The OAuth access token for your Slack app"
+}
+
+variable "basecamp_client_id" {
+  description = "The client ID for your Basecamp app"
+}
+
+variable "basecamp_client_secret" {
+  description = "The client secret for your Basecamp app"
+}
